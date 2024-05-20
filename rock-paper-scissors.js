2b4c3a872108683f0c0c094f57349c858eeb80b4
Tobi-007 <tobioketade007@gmail.com>
2024-05-20 13:56:33 -0700

    modification made: telling user number of rounds

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,6 @@
 var userChoice = prompt("Would you like to play a game of rock,paper,scissors? [y/n]",'');
 if(userChoice === 'y'){
+  console.log("There are five rounds!!!");
   playGame()
 } else if(userChoice === 'n'){
     console.log("Reload the page and type in y in case you change your mind");
@@ -39,22 +40,22 @@ function playGame(){
     let HC = humanChoice.toUpperCase();
     let CC = computerChoice.toUpperCase(); 
       if (HC === "ROCK" && CC === "PAPER"){
-        console.log( "%cYou lose! Paper beats Rock.","color: red;");
+        console.log( "%cYou lose this round! Paper beats Rock.","color: red;");
         return computerScore ++;
       } else if (HC === "PAPER" && CC === "SCISSORS"){
-        console.log( "%cYou lose! Scissors beats Paper.","color: red;");
+        console.log( "%cYou lose this round! Scissors beats Paper.","color: red;");
         return computerScore ++;
       } else if (HC === "SCISSORS" && CC === "ROCK"){
-        console.log( "%cYou lose! Rock beats Scissors.","color: red;");
+        console.log( "%cYou lose this round! Rock beats Scissors.","color: red;");
         return computerScore ++;
       } else if (CC === "ROCK" && HC === "PAPER"){
-        console.log( "%cYou win! Paper beats Rock.","color: green; font-weight: bold;");
+        console.log( "%cYou win this round! Paper beats Rock.","color: green; font-weight: bold;");
         return humanScore ++;
       } else if (CC === "PAPER" && HC === "SCISSORS"){
-        console.log( "%cYou win! Scissors beats Paper.","color: green; font-weight: bold;");
+        console.log( "%cYou win this round! Scissors beats Paper.","color: green; font-weight: bold;");
         return humanScore ++;
       } else if (CC === "SCISSORS" && HC === "ROCK"){
-        console.log( "%cYou win! Rock beats Scissors.","color: green; font-weight: bold;");
+        console.log( "%cYou win this round! Rock beats Scissors.","color: green; font-weight: bold;");
         return humanScore ++;
       } else if(CC === HC){
         console.log( `%cNo Winner this time!\nYou both chose ${humanChoice}.`,"color: blue;");
@@ -73,7 +74,7 @@ function playGame(){
     } else if(computerScore < humanScore){
         console.log(`%cWinner: Human with a total score of ${humanScore} while Computer lost with a score of ${computerScore}`,"color: green; font-weight: bolder;");
     } else if(computerScore === humanScore){
-        console.log(`%cNo Winner this time, You both had a score of ${humanScore}!`,"color: brown;");
+        console.log(`%cIt was a tie!, You both had a score of ${humanScore}!`,"color: brown;");
         userChoice;
     } 
 }
