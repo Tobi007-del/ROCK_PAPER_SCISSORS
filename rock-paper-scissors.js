d6e97259d46367ff6263531a12bf013010ed2185
Tobi-007 <tobioketade007@gmail.com>
2024-05-23 16:12:54 -0700

    minor changes

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,7 @@ var userChoice = prompt("Would you like to play a game of rock,paper,scissors?",
 if(userChoice.toLowerCase() === 'y' || userChoice.toUpperCase() === 'YES'){
   console.log("There are five rounds!!!");
   playGame()
-} else if(userChoice.toLowerCase() === 'n' || userChoice.toUpperCase() === 'NO'){
+} else if(userChoice.toLowerCase(cl) === 'n' || userChoice.toUpperCase() === 'NO'){
     console.log("Reload the page and type in y in case you change your mind");
 } else{
     console.log("I hope you change your mind!");
